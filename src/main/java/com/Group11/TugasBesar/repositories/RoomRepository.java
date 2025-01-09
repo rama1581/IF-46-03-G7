13520f4f14073511471b4eb669fe0f2e59ceb1eb
Muhammad Zidan Ramadhan <147800862+rama1581@users.noreply.github.com>
2025-01-09 14:05:17 +0700

    Menambah 1 List Pada RoomRepository.java

--- src/main/java/com/Group11/TugasBesar/repositories/RoomRepository.java
+++ src/main/java/com/Group11/TugasBesar/repositories/RoomRepository.java
@@ -1,9 +1,9 @@
 package com.Group11.TugasBesar.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 import com.Group11.TugasBesar.models.Kost;
 import com.Group11.TugasBesar.models.Room;
@@ -14,4 +14,6 @@ public interface RoomRepository extends JpaRepository<Room, Integer>{
     List<Room> findByKost(Kost kost);
 
     List<Room> findByBooked(boolean booked);
+
+    List<Room> findByType(String type);
 }
