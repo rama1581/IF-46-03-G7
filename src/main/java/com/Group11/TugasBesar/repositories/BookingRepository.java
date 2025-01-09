43e5525a7c5db06ffd0aa920fa79f69304c44134
Muhammad Zidan Ramadhan <147800862+rama1581@users.noreply.github.com>
2025-01-09 14:04:03 +0700

    Tamabahn Sedikit Pada BookingRepository.java

--- src/main/java/com/Group11/TugasBesar/repositories/BookingRepository.java
+++ src/main/java/com/Group11/TugasBesar/repositories/BookingRepository.java
@@ -1,8 +1,9 @@
 package com.Group11.TugasBesar.repositories;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Group11.TugasBesar.models.Booking;
 import com.Group11.TugasBesar.models.PencariKost;
@@ -13,4 +14,5 @@ public interface BookingRepository extends JpaRepository<Booking, Integer>{
     Optional<Booking> findByRoom(Room room);
 
     List<Booking> findByPencariKost(PencariKost pencariKost);
+
 }
