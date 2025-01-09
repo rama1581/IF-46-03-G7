91f9688ff5aef145ae26c52ff46e7cef7ce22ee6
Muhammad Zidan Ramadhan <147800862+rama1581@users.noreply.github.com>
2025-01-09 14:04:53 +0700

    Menambah Balance Pada PemilikKostRepository.java

--- src/main/java/com/Group11/TugasBesar/repositories/PemilikKostRepository.java
+++ src/main/java/com/Group11/TugasBesar/repositories/PemilikKostRepository.java
@@ -1,6 +1,8 @@
 package com.Group11.TugasBesar.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Group11.TugasBesar.models.PemilikKost;
@@ -9,5 +11,6 @@ import com.Group11.TugasBesar.models.PemilikKost;
 public interface PemilikKostRepository extends JpaRepository<PemilikKost, Integer>{
     
     // Optional<PemilikKost> findByName(String name);
-    
+    @Query(value = "SELECT balance FROM pemilik_kost WHERE pemilik_kost_id = :pemilikKostId", nativeQuery = true)
+    Long findBalanceByPemilikKostId(@Param("pemilikKostId") Integer pemilikKostId);
 }
