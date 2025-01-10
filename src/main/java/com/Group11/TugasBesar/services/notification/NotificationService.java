da0ecc0d4873b820a9bd4cd4a9d830a0926b6e81
Azett <auliazzahraa28@gmail.com>
2025-01-10 20:38:48 +0700

    Update NotificationService.java

--- src/main/java/com/Group11/TugasBesar/services/notification/NotificationService.java
+++ src/main/java/com/Group11/TugasBesar/services/notification/NotificationService.java
@@ -11,4 +11,7 @@ public interface NotificationService {
     Response getNotifications();
 
     Response getNotificationByPencariKost(PencariKost pencariKost);
+
+    Response getNotificationsByConfirmedBookings(int pencariKostId);
+
 }
