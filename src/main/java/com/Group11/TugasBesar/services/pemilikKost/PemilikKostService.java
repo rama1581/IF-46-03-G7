77e5fc550ce9c1a45c27a625f58ffe4786c5d7a2
Azett <auliazzahraa28@gmail.com>
2025-01-10 20:36:15 +0700

    Update PemilikKostService.java

--- src/main/java/com/Group11/TugasBesar/services/pemilikKost/PemilikKostService.java
+++ src/main/java/com/Group11/TugasBesar/services/pemilikKost/PemilikKostService.java
@@ -1,5 +1,6 @@
 package com.Group11.TugasBesar.services.pemilikKost;
 
+import com.Group11.TugasBesar.models.Payment;
 import com.Group11.TugasBesar.payloads.requests.UserRequest;
 import com.Group11.TugasBesar.payloads.responses.Response;
 
@@ -8,6 +9,11 @@ public interface PemilikKostService {
 	Response addPemilikKost(UserRequest request);
 
 	Response getPemilikKost();
+
+	Response transferToPemilikKost(Payment payment);
+
+	long getBalanceByPemilikKostId(int pemilikKostId);
+
 
 	// Response getPemilikKostByName(String name);
 }
